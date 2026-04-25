0427dad3da68f628c6d76ff0f23799d6df762064
abhisheksingh0196 <97225179+abhisheksingh0196@users.noreply.github.com>
2026-04-25 04:16:08 +0000

    error handling and separating database creds

---
+++ config.js
@@ -0,0 +1,5 @@
+// config.js
+const CONFIG = {
+    SUPABASE_URL: 'https://utocmozaaxajyqxtjzot.supabase.co',
+    SUPABASE_KEY: 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV0b2Ntb3phYXhhanlxeHRqem90Iiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzcwMzQ1MDgsImV4cCI6MjA5MjYxMDUwOH0.GSm-Kud_3Yy3R1UyceZetpLoHQ701bp5ybMSHalSbnA'
+};
